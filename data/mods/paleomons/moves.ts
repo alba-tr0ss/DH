783b5e44f280181a0da8b1270ce519440b8226cb
Albatross <83423061+alba-tr0ss@users.noreply.github.com>
2022-03-12 11:15:25 +0200

    Thunderstruck #2

--- data/mods/paleomons/moves.ts
+++ data/mods/paleomons/moves.ts
@@ -398,4 +398,30 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 			}
 		},
 	},
+
+	grassyterrain: {
+		inherit: true,
+		condition: {
+
+			onBasePower(basePower, attacker, defender, move) {
+				const weakenedMoves = ['earthquake', 'bulldoze', 'magnitude'];
+				if (weakenedMoves.includes(move.id) && (attacker.ability != "Thunderstruck" || defender.ability != "Thunderstruck")) {
+					this.debug('move weakened by grassy terrain');
+					return this.chainModify(0.5);
+				}
+				if (move.type === 'Grass' && attacker.isGrounded() && (attacker.ability != "Thunderstruck" || defender.ability != "Thunderstruck")) {
+					this.debug('grassy terrain boost');
+					return this.chainModify([0x14CD, 0x1000]);
+				}
+			},
+
+			onTerrain(pokemon) {
+				if (pokemon.isGrounded() && !pokemon.isSemiInvulnerable()) {
+					this.debug('Pokemon is grounded, healing through Grassy Terrain.');
+					this.heal(pokemon.baseMaxhp / 16, pokemon, pokemon);
+				}
+				if(pokemon.ability == "Thunderstruck") return;
+			},
+		}
+	},
 };
