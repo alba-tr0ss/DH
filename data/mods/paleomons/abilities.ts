32711dce2461963f0e54e01b6bca9d97a6b5adaf
Albatross <83423061+alba-tr0ss@users.noreply.github.com>
2022-04-08 23:15:13 +0300

    garchompeee

--- data/mods/paleomons/abilities.ts
+++ data/mods/paleomons/abilities.ts
@@ -248,9 +248,12 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 	},
 
 	persistence: {
-		onHit(target, source, move) {
-			if (move.category !== 'Status') return;
-			if (source.moveLastTurnResult === false) {
+		onHit(pokemon, target, move) {
+			if (move.category === 'Status') return;
+			const damagedTarget = target.attackedBy.some(
+				p => p.source === pokemon && p.damage > 0 && p.thisTurn
+			);
+			if (!damagedTarget) {
 				this.boost({atk: 1});
 			}
 		},
