b63fd328b5ac393d98fbc00b3186d33b75e58eb9
Albatross <83423061+alba-tr0ss@users.noreply.github.com>
2022-04-08 17:07:23 +0300

    Tyranitar #6

--- data/mods/paleomons/moves.ts
+++ data/mods/paleomons/moves.ts
@@ -659,4 +659,25 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		target: "normal",
 		type: "Water",
 	},
+
+	naturalgift: {
+		inherit: true,
+		onModifyType(move, pokemon) {
+			if (pokemon.ignoringItem()) return;
+			const item = pokemon.getItem();
+			if (!item.naturalGift) return;
+			move.type = item.naturalGift.type;
+		},
+		onPrepareHit(target, pokemon, move) {
+			if (pokemon.ignoringItem()) return false;
+			const item = pokemon.getItem();
+			if (!item.naturalGift) return false;
+			move.basePower = item.naturalGift.basePower;
+			if (pokemon.hasAbility('natureprowess')) return;
+			pokemon.setItem('');
+			pokemon.lastItem = item.id;
+			pokemon.usedItemThisTurn = true;
+			this.runEvent('AfterUseItem', pokemon, null, null, item);
+		},
+	},
 };
