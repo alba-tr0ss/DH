e6ce49f67c3e151dfd8127fc4fc79709d0f723e3
Albatross <83423061+alba-tr0ss@users.noreply.github.com>
2022-07-07 00:38:13 +0300

    sync these nuts

--- data/mods/legendshoopa/abilities.ts
+++ data/mods/legendshoopa/abilities.ts
@@ -11,6 +11,7 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 			if (!source || source === target) return;
 			if (effect && effect.id === 'toxicspikes') return;
 			this.add('-activate', target, 'ability: Synchronize');
+			this.add('-curestatus', target, {status: status.id, id: 'synchronize'} as Effect, '[Silent]');
 			target.setStatus('');
 			// Hack to make status-prevention abilities think Synchronize is a status move
 			// and show messages when activating against it.
