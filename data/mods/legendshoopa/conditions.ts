31a07a1a468b6e83203d80a4301577b8d573f353
Albatross <83423061+alba-tr0ss@users.noreply.github.com>
2022-07-14 15:49:38 +0300

    h

--- data/mods/legendshoopa/conditions.ts
+++ data/mods/legendshoopa/conditions.ts
@@ -83,7 +83,7 @@ export const Conditions: {[k: string]: ConditionData} = {
 
 		onResidual(pokemon) {
 			const jaggedHazard = this.dex.getActiveMove('Stealth Rock');
-			jaggedHazard.type = this.effectData.jaggedType.type;
+			jaggedHazard.type = this.effectData.jaggedType;
 			const typeMod = this.clampIntRange(pokemon.runEffectiveness(jaggedHazard), -6, 6);
 			const damage = this.getDamage(pokemon, pokemon, 25);
 			if (typeof damage !== 'number') throw new Error("Jagged Splinters damage not dealt");
